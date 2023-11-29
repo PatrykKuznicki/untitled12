4427eb40b27de79e209c164ce5646972877f88e9
PatrykKuznicki <252808@edu.p.lodz.pl>
2023-11-29 15:17:33 +0100

    Document update

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
 int Policz(int a, int b){
-    return a + b;
+    return a * b;
 }
 int main() {
     std::cout << "Hello, World1234512345!" << std::endl;
-    std::cout << "First branch; 2 + 3 + " <<Policz(2 , 3) << std::endl;
+    std::cout << "First branch; 2 * 3 + " <<Policz(2 , 3) << std::endl;
     return 0;
 }
